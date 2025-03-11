58a6c2fca75b011adccac57164d905447d08626a
Arslane101 <67836447+Arslane101@users.noreply.github.com>
2025-03-11 12:53:43 +0100

    Added option to add link instead of uploading

--- OpenAiWisper.py
+++ OpenAiWisper.py
@@ -1,7 +1,7 @@
-from io import StringIO
+from io import BytesIO
 from time import sleep
+import urllib.request
 import assemblyai as aai
-
 import requests
 import streamlit as st
 from openai import OpenAI
@@ -20,9 +20,12 @@ def Gladia(uploaded_file):
     "x-gladia-key": st.secrets["GLADIA_API_KEY"],  # Replace with your Gladia Token
     "accept": "application/json",
     }
-
+    if not toggle: 
+       uploaded_file = BytesIO(requests.get(uploaded_file).content)
+       uploaded_file.name = "audio.webm"
+       print(uploaded_file)
     files = {
-        "audio": (uploaded_file.name, uploaded_file, uploaded_file.type)
+        "audio": (uploaded_file.name, uploaded_file, "audio/webm")
     }
     print("- Uploading file to Gladia...")
     upload_response = make_request(
@@ -55,13 +58,14 @@ def Gladia(uploaded_file):
         else:
             reply = poll_response.get("status")
         sleep(1)
-    
     return reply["transcription"]["full_transcript"]
 @st.cache_data
 def Deepgram(uploaded_file,input):
    deepgram = DeepgramClient(st.secrets["secret"])  
    # STEP 2 Call the transcribe_file method on the rest class
- 
+   if not toggle: 
+       uploaded_file = BytesIO(requests.get(uploaded_file).content)
+       uploaded_file.name = "audio.webm"
    payload: FileSource = {
             "buffer": uploaded_file,
             "mimetype": "video/webm"
@@ -73,14 +77,16 @@ def Deepgram(uploaded_file,input):
    )
    file_response = deepgram.listen.rest.v("1").transcribe_file(payload, options,timeout = 300)
    return file_response['results']['channels'][0]['alternatives'][0]['transcript']
+
 @st.cache_data
 def Assembly(uploaded_file):
 
     aai.settings.api_key = st.secrets["ASSEMBLY_API_KEY"]
     transcriber = aai.Transcriber()
-
-    audio_file = uploaded_file.getvalue()
-
+    if not toggle: 
+       uploaded_file = BytesIO(requests.get(uploaded_file).content)
+       uploaded_file.name = "audio.webm"
+    audio_file = uploaded_file
     config = aai.TranscriptionConfig(language_detection=True)
 
     transcript = transcriber.transcribe(audio_file, config)
@@ -91,41 +97,58 @@ def Assembly(uploaded_file):
 
     return transcript.text
 @st.cache_data
-def Whisper(uploaded_file):
-    transcription = client.audio.transcriptions.create(
+def Whisper(uploaded_file): 
+    client = OpenAI(api_key = st.secrets["OPENAI_API_KEY"])
+    if  toggle:
+        transcription = client.audio.transcriptions.create(
             model="whisper-1",
             file=uploaded_file,
             )
+    else:
+        audio = BytesIO(requests.get(uploaded_file).content)
+        audio.name = "audio.webm"
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio,
+            )
     return transcription.text
 
+
 st.title("Transcription Testing")
-model = st.selectbox("Select the service to use for transcription", ["Assembly", "Deepgram", "Gladia","Whisper"], 
-             key="model",index=None,
-    placeholder="eg : Whisper")
-if(model=="Deepgram"):
-    input = st.text_input("Type the name of the model you want to use : ",value="nova-2",placeholder="eg : nova-2")
-# Set Model
-client = OpenAI(api_key = st.secrets["OPENAI_API_KEY"])
+
+
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 
-
 # Accept user input
-uploaded_file = st.file_uploader("Insert Recording", type="webm")
-if uploaded_file is not None:
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": uploaded_file})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown("File Uploaded with Success ! ")
-    # Transcribe the audio file
-    with st.chat_message("assistant"):
-       if (model=="Deepgram"):
+toggle = st.toggle(label="📁")
+response = "No response yet"
+if toggle:
+    uploaded_file = st.file_uploader("Insert Recording", type="webm")
+else: 
+    uploaded_file = st.text_input("Insert Link",placeholder="eg: https://www.example.com/recording.webm")
+model = st.selectbox("Select the service to use for transcription", ["Assembly", "Deepgram", "Gladia","Whisper"],
+    placeholder="eg : Whisper",index=None)
+if(model=="Deepgram"):
+    input = st.text_input("Type the name of the model you want to use : ",value="nova-2",placeholder="eg : nova-2")
+if(uploaded_file is not None):
+ st.session_state.messages.append({"role": "user", "content": uploaded_file})
+ if toggle : 
+     with st.chat_message("user"):
+        st.markdown("File Uploaded with Success !")
+ else:
+     with st.chat_message("user"):
+        st.markdown("Link Successfully Added ! ")
+ with st.chat_message("assistant"):
+     if (model=="Deepgram"):
             response = Deepgram(uploaded_file,input)
-       else:
-           response = eval(model)(uploaded_file)
-       st.write(response)
-       st.session_state.messages.append({"role": "assistant", "content": response})
+     else:
+           if(model is not None):
+               response = eval(model)(uploaded_file)
+st.write(response)
+st.session_state.messages.append({"role": "assistant", "content": response})
+
+
