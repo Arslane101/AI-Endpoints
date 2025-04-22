d9de54a70cfa4b23c4a85a719bbc64d2a65af108
Arslane101 <rahalabdelhamid@gmail.com>
2025-04-22 21:43:15 +0100

    wallahi it' the last

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 # If you want to use the no-sections version, this
 # defaults to looking in .streamlit/pages.toml, so you can
 # just call `get_nav_from_toml()`
-nav = get_nav_from_toml()
+nav = get_nav_from_toml(".streamlit/pages_sections.toml")
 
 st.logo("images.jpg")
 
